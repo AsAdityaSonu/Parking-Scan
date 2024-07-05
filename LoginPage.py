13d3741979279fd5923adf90ee6a203290b2cee4
Aditya Pandey <105072228+AsAdityaSonu@users.noreply.github.com>
2024-07-06 00:17:23 +0530

    file structure maintained

--- LoginPage.py
+++ LoginPage.py
@@ -1,5 +1,83 @@
 import tkinter.messagebox as tkmb
+import customtkinter as ctk
+from PIL import Image
 from MainPage import Dashboard
+
+
+def loginScreen(app, frame_width, frame_height, frame_x, frame_y):
+    frame = ctk.CTkFrame(
+        master=app,
+        width=frame_width,
+        height=frame_height,
+        border_width=2,
+        border_color="#404040"
+    )
+    frame.place(x=frame_x, y=frame_y, anchor='center')
+
+    label = ctk.CTkLabel(app, text="Welcome to the Pehal Portal!", font=("Helvetica", 36, "bold"))
+    label.pack(pady=60)
+
+    image_path = "/Users/adityapandey/GUI/pythonProject/Tkinter/7.jpg"
+    try:
+        image = Image.open(image_path)
+        ctk_image = ctk.CTkImage(light_image=image, dark_image=image, size=(100, 100))
+
+        header_label = ctk.CTkLabel(master=frame, image=ctk_image, text='')
+        header_label.pack(pady=(50, 0))
+    except FileNotFoundError:
+        tkmb.showerror("Error", f"Image not found at {image_path}")
+
+    container = ctk.CTkFrame(master=frame, fg_color="transparent")
+    container.pack(expand=True, fill='both', padx=50, pady=(30, 50))
+
+    user_entry = ctk.CTkEntry(
+        master=container,
+        placeholder_text="Username",
+        font=("Helvetica", 16),
+        width=300,
+        height=40,
+        corner_radius=10,
+        border_width=2,
+        border_color="gray",
+        fg_color="white",
+        # bg_color="lightblue",
+        text_color="black",
+        placeholder_text_color="gray"
+    )
+    user_entry.pack(pady=12)
+
+    user_pass = ctk.CTkEntry(
+        master=container,
+        placeholder_text="Password",
+        show="*",
+        font=("Helvetica", 16),
+        width=300,
+        height=40,
+        corner_radius=10,
+        border_width=2,
+        border_color="gray",
+        fg_color="white",
+        # bg_color="lightblue",
+        text_color="black",
+        placeholder_text_color="gray"
+    )
+    user_pass.pack(pady=12)
+
+    login_button = ctk.CTkButton(
+        master=container,
+        text='Login',
+        command=lambda: login(app, user_entry, user_pass),
+        font=("Helvetica", 16, "bold"),
+        width=300,
+        height=40,
+        corner_radius=10,
+        border_width=2,
+        border_color="gray",
+        fg_color="lightblue",
+        hover_color="lightblue",
+        text_color="black"
+    )
+    login_button.pack(pady=12)
 
 
 def login(app, user_entry, user_pass):
--- ParkingScan.py
+++ ParkingScan.py
@@ -1,7 +1,9 @@
+'''
+    This file is the entry point for the Parking Scan application.
+'''
+
 import customtkinter as ctk
-import tkinter.messagebox as tkmb
-from PIL import Image, ImageTk
-from LoginPage import login
+from LoginPage import loginScreen
 
 
 def set_theme():
@@ -21,39 +23,9 @@ def main_window(app):
     frame_width = 500
     frame_height = 600
     frame_x = screen_width // 2
-    frame_y = (screen_height-100) // 2
+    frame_y = (screen_height - 100) // 2
 
-    frame = ctk.CTkFrame(master=app, width=frame_width, height=frame_height)
-    frame.place(x=frame_x, y=frame_y, anchor='center')
-
-    label = ctk.CTkLabel(app, text="Welcome to Pehal", font=("Helvetica", 24, "bold"))
-    label.pack(pady=50)
-
-    image_path = "/Users/adityapandey/GUI/pythonProject/Tkinter/7.jpg"
-    try:
-        image = Image.open(image_path)
-        image = image.resize((100, 100))
-        img = ImageTk.PhotoImage(image)
-
-        # Image label
-        header_label = ctk.CTkLabel(master=frame, image=img, text='')
-        header_label.image = img
-        header_label.pack(pady=(50, 0))
-    except FileNotFoundError:
-        tkmb.showerror("Error", f"Image not found at {image_path}")
-
-    container = ctk.CTkFrame(master=frame, fg_color="transparent")
-    container.pack(expand=True, fill='both', padx=50, pady=50)
-
-    user_entry = ctk.CTkEntry(master=container, placeholder_text="Username", font=("Helvetica", 16), width=300)
-    user_entry.pack(pady=12)
-
-    user_pass = ctk.CTkEntry(master=container, placeholder_text="Password", show="*", font=("Helvetica", 16), width=300)
-    user_pass.pack(pady=12)
-
-    login_button = ctk.CTkButton(master=container, text='Login', command=lambda: login(app, user_entry, user_pass),
-                                 font=("Helvetica", 16, "bold"))
-    login_button.pack(pady=12)
+    loginScreen(app, frame_width, frame_height, frame_x, frame_y)
 
 
 def main():
